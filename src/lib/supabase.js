3e8ecaf2c0a330b611d62d1610a0b0071bfee726
ManjuShashankPavan <luxuryforeverydayneeds@gmail.com>
2025-03-08 00:50:26 +0530

    Authentication

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://iozerhwhnutidhkgmcpm.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvemVyaHdobnV0aWRoa2dtY3BtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzNDkyNTYsImV4cCI6MjA1NjkyNTI1Nn0.N2APZaPezUrvNb9Y2cpdJn1T4JhHqqR74snGXf_VDvc";
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
