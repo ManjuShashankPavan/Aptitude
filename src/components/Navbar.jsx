e8f36cb9459025c8af3e817cf3435a86cf0dae81
MANJUNATH <manjunathgowda1004@gmail.com>
2025-03-08 16:22:27 +0530

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ export default function Navbar({ setShowSignIn, setShowSignUp }) {
   };
 
   return (
-    <nav className="bg-blue-600 p-4 shadow-md">
+    <nav className="bg-blue-600  p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <a href="/">
